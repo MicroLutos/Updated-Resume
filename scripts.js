c1d4f2c62057ba70719ffda985c9956528126c91
MicroLutos <johnjeffersonmamuad16@gmail.com>
2024-06-12 02:30:28 -0700

    scripts.js

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ function printResume() {
 }
 
 function downloadPDF() {
-    const element = document.querySelector('.container');
+    const element = document.querySelector('.container'); // Select the container element containing your resume content
     html2pdf()
         .from(element)
-        .save('resume.pdf');
+        .save('John_Jefferson_A_Mamuad_Resume.pdf');
 }
